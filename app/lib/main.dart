f8a96d55150a7430ca7f04c20d5707449871561b
Andrey Melnikov <a.melnikov4@g.nsu.ru>
2026-09-02 12:18:13 +0700

    Fix app didn't run

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
 import 'src/app.dart';
 import 'src/rust/frb_generated.dart';
@@ -6,6 +7,8 @@ import 'src/rust/frb_generated.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Load the native `docer-core` library and initialize the bridge.
-  await RustLib.init();
+  await RustLib.init(
+    externalLibrary: ExternalLibrary.process(iKnowHowToUseIt: true),
+  );
   runApp(const DocerApp());
 }
